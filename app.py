1d5e88b911277f824d2d6cd14317909f4a87fbca
itsYali <156930280+itsYali@users.noreply.github.com>
2026-06-13 14:41:38 +0800

    Add files via upload
    Initial upload of project files

---
+++ app.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import os
+import socket
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {
+        "status": "Online",
+        "pod_name": os.getenv("HOSTNAME", "Unknown-Pod"),
+        "pod_ip": socket.gethostbyname(socket.gethostname()),
+        "message": "MicroK8s stateless service operational"
+    }
+
+@app.get("/healthz")
+def health_check():
+    return {"status": "healthy"}
